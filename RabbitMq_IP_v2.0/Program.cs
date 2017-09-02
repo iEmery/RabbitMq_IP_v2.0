46b51d8eccc26e8971abc820a4c8d166c1a37b58
emery ishimwe <emery.ishimwe@student.ehb.be>
2017-09-02 17:32:53 +0200

    start project
    first commit: empty project

---
+++ RabbitMq_IP_v2.0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMq_IP_v2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
